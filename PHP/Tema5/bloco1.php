964595c0a536b5e8c03d7c045cba7711f52e6b3d
Juliano Silva <57194781+JulianoSilva05@users.noreply.github.com>
2024-04-30 22:13:07 -0300

    Funcoes_PHP

---
+++ PHP/Tema5/bloco1.php
@@ -0,0 +1,16 @@
+<?php
+/*Funções em PHP*/
+/*
+Exemplo:
+function nome_da_funcao(parametrou){
+    bloco de codigo
+}
+*/
+function soma($a, $b){
+    retorn $a + $b;
+}
+
+/*Chamar a função*/
+echo soma(2,5);
+
+?>
